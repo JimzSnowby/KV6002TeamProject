c3694e0e26320b637e82448c961b56d9a0ad7652
JimzSnowby <117113270+JimzSnowby@users.noreply.github.com>
2024-03-08 10:30:44 +0000

    changed db stuff

--- api/config/settings.php
+++ api/config/settings.php
@@ -6,6 +6,6 @@
  * @author James Sowerby w21023500
  */
 define('BASEPATH', '/assessment/api');
-define('CHI2023_DATABASE', 'db/chi2023.sqlite');
+define('MAIN_DATABASE', 'db/teamProject.sqlite');
 define('USERS_DATABASE', 'db/users.sqlite');
 define('SECRET', 'B8*}#d&~0V$.=;iQOQG;694;8/&D*bBzbA,m9X<w;t~?On"t/@bcH16y7lL2a)i'); 
