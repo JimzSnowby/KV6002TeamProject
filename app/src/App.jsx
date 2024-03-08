f92e679e5a948c9793a2148d768e015d029c6979
JimzSnowby <117113270+JimzSnowby@users.noreply.github.com>
2024-03-08 14:56:56 +0000

    React

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import HomePage from "./pages/HomePage";
 import './App.css'
 
 function App() {
--- app/src/main.jsx
+++ app/src/main.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
+
+/**
+ * The main entry point for the application.
+ * 
+ * @author James Sowerby w21023500
+ */
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename=''>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
