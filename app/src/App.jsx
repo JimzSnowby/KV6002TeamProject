dd74da8ef20015e05f1f076866c306298d97ba80
Maja <maja.bosy@northumbria.ac.uk>
2024-03-15 17:11:46 +0000

    dd

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,42 +3,18 @@ import { useState } from 'react'
 import HomePage from "./pages/HomePage"
 import Menu from "./components/Menu"
 import Footer from "./components/Footer"
-import Register from "./pages/Register"
-import SignIn from "./pages/SignIn"
+import SignIn from "./components/SignIn"
 import VolunteerPage from "./pages/VolunteerPage"
-import Chatbot from 'react-chatbot-kit'
+import ParticipantPage from "./pages/ParticipantPage"
 
-import ActionProvider from "./config/ActionProvider"
-import MessageParser from "./config/MessageParser"
-import config from "./config/config"
-import 'react-chatbot-kit/build/main.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
   const [signedIn, setSignedIn] = useState(false)
-<<<<<<< HEAD
-
-  return (
-    <>
-    <div className="p-10">
-      <SignIn
-        signedIn={signedIn}
-        setSignedIn={setSignedIn}
-        table="volunteer"
-        tableID="volunteerID"
-      />
-    </div>
-    <nav className="px-5">
-      <Menu />
-      {/*<Chatbot config={config} actionProvider={ActionProvider} 	    messageParser={MessageParser} />*/}
-    </nav>
-    <Routes>
-=======
   const [roletype, setRoletype] = useState('')
 
   console.log("Final Role type:", roletype);
-
-
 
   return (
     <>
@@ -54,24 +30,15 @@ function App() {
         <Menu roletype={roletype} signedIn={signedIn}  />
       </nav>
       <Routes>
->>>>>>> main
         <Route path="/" element={<HomePage />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/register" element={<Register />} />
         {/* Put If statement for the profile, checking for logged in user account type */}
        
           <Route path="/volunteer" element={<VolunteerPage />} />
-<<<<<<< HEAD
-    </Routes>
-    <div>
-=======
-        
-
+      
        
           <Route path="/participant" element={<ParticipantPage />} />
       </Routes>
       <div className="px-5">
->>>>>>> main
         <Footer />
       </div>
     </>
