2d601e425025acd85e5763d9acf298e2d291320e
ScytheNight <scythenight@protonmail.com>
2024-03-17 15:33:30 +0000

    basepath

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -14,7 +14,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename=''> 
+    <BrowserRouter basename='/assessment/api'> 
       <App />
     </BrowserRouter>
   </React.StrictMode>,
