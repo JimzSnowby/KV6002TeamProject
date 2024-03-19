453100e7eed29e2aed855c4f6dc12dd10cab9be9
JimzSnowby <117113270+JimzSnowby@users.noreply.github.com>
2024-03-19 17:48:45 +0000

    Update VolunteerPage.jsx

--- app/src/pages/VolunteerPage.jsx
+++ app/src/pages/VolunteerPage.jsx
@@ -7,17 +7,35 @@ import { useState, useEffect } from 'react'
  */
 function VolunteerPage(props) {
     const [details, setDetails] = useState([])
+    const [availability, setAvailability] = useState({
+        Monday: false,
+        Tuesday: false,
+        Wednesday: false,
+        Thursday: false,
+        Friday: false,
+        Saturday: false,
+        Sunday: false,
+    })
 
     useEffect(() => {
         console.log("Volunteer ID: ", props.userID)
+        fetchDetails()
+
+    }, [])
+
+    const fetchDetails = () => {
         fetch('https://w21023500.nuwebspace.co.uk/assessment/api/volunteer?volunteerID=' + props.userID)
         .then(response => response.json())
         .then(data => setDetails(data))
         .catch(error => console.error(error))
-    }, [])
+    }
+
+    const fetchAvailability = () => {
+        
+    }
 
     const detailsJSX = details.map((details, index) => 
-        <section key = {index} className='p-5 bg-blue-200'>
+        <section key = {index} className='flex-1 p-5 bg-blue-200'>
             <h2 className='text-2xl text-center'>{details.name}</h2>
             <p className='text-center'>Volunteer ID: {details.volunteerID}</p>
             <p className='text-center'>Date of Birth: {details.dob}</p>
@@ -26,10 +44,15 @@ function VolunteerPage(props) {
         )
 
     return (
-        <section className='p-5'>
+        <>
             <h1 className='text-4xl text-center'>Profile</h1>
-            {detailsJSX}
-        </section>
+            <div className='p-5 flex'>
+                {detailsJSX}
+                <div className='flex-1 p-5'>
+                    <p className='text-2xl text-center'>Availability</p>
+                </div>
+            </div>
+        </>
     )
 }
 
