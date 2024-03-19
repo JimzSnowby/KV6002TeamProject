0d7c7c50a7ac8d47a6906327d6faf912b8cbb4e1
JimzSnowby <117113270+JimzSnowby@users.noreply.github.com>
2024-03-19 21:12:06 +0000

    Fixed Events

--- app/src/pages/VolunteerPage.jsx
+++ app/src/pages/VolunteerPage.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import Event from '../components/Event'
 
 /**
  * Page to manage Volunteers profile
@@ -7,6 +8,14 @@ import { useState, useEffect } from 'react'
  */
 function VolunteerPage(props) {
     const [details, setDetails] = useState([])
+    const [event, setEvent] = useState([])
+    const [page, setPage] = useState(1)
+    const [extendEvent, setextendEvent] = useState(null)
+
+    const itemsPerPage = 10
+    const startOfPage = (page - 1) * itemsPerPage
+    const endOfPage = startOfPage + itemsPerPage
+
     const [availability, setAvailability] = useState({
         Monday: false,
         Tuesday: false,
@@ -20,37 +29,63 @@ function VolunteerPage(props) {
     useEffect(() => {
         console.log("Volunteer ID: ", props.userID)
         fetchDetails()
-
+        fetchEvents()
     }, [])
-
+    
     const fetchDetails = () => {
         fetch('https://w21023500.nuwebspace.co.uk/assessment/api/volunteer?volunteerID=' + props.userID)
         .then(response => response.json())
         .then(data => setDetails(data))
-        .catch(error => console.error(error))
+        .catch(error => console.error("Error fetching details: ", error))
     }
 
     const fetchAvailability = () => {
         
     }
 
+    const fetchEvents = () => {
+        fetch('https://w20021570.nuwebspace.co.uk/assessment/api/event')
+        .then(response => response.json())
+        .then(data => setEvent(data))
+        .catch(error => console.error("Error fetching events: ", error))
+    }
+
     const detailsJSX = details.map((details, index) => 
         <section key = {index} className='flex-1 p-5 bg-blue-200'>
-            <h2 className='text-2xl text-center'>{details.name}</h2>
+            <p className='text-2xl text-center'>{details.name}</p>
             <p className='text-center'>Volunteer ID: {details.volunteerID}</p>
             <p className='text-center'>Date of Birth: {details.dob}</p>
             <p className='text-center'>Email: {details.email}</p>
         </section>
-        )
+    )
+
+    const eventsJSX = event.slice(startOfPage, endOfPage)    
+                            .map((item) =>(
+        <section 
+            key = {item.eventID}
+            className={`p-4 m-2 rounded-lg border border-gray-300 ${extendEvent === item.eventID ? 'md:col-span-2' : 'col-span-1'
+                    }`}>
+            <Event
+                event={item}
+                signedIn={props.signedIn}
+                extendEvent={extendEvent}
+                setextendEvent={setextendEvent}
+            />
+        </section>
+    ))
 
     return (
         <>
-            <h1 className='text-4xl text-center'>Profile</h1>
+            <h1 className='text-4xl text-center p-2'>Profile</h1>
             <div className='p-5 flex'>
                 {detailsJSX}
                 <div className='flex-1 p-5'>
                     <p className='text-2xl text-center'>Availability</p>
                 </div>
+            </div>
+            <div className='justify-center p-5'>
+                    <p className='text-2xl text-center'>Upcoming Events</p>
+                    {eventsJSX}
             </div>
         </>
     )
